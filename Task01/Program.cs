9756aafabe9d833e146bb88bc5cadb0c2b2f683e
Lekxpa <LekxpaP@gmail.com>
2022-10-03 01:42:45 +0300

    add decidion in file Task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -9,3 +9,45 @@
 // [“hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] MyArray (int Array)
+{
+    string[] NArray = new string[Array];
+    for (int i = 0; i < Array; i++)
+    {
+        Console.Write($"Введите {i+1} значение : ");
+        NArray[i] = Console.ReadLine();
+    }
+    return NArray;
+}
+
+void PrintArray(string[] array)
+{
+    Console.WriteLine(String.Join(" ", array));
+}
+
+string[] CalculationOfSymbol(string[] COSArray)
+{
+    string[] NewArray = new string[COSArray.Length];
+    int Length = 3;
+    int SizeArray = 0;
+    for (int j = 0; j < COSArray.Length; j++)
+    {
+        if (COSArray[j].Length <= Length)   
+        {
+            NewArray[SizeArray] = COSArray[j];
+            SizeArray++;
+        }
+    }
+   return NewArray;
+}    
+
+Console.Write("Введите количество значений в массиве: ");
+int values = int.Parse(Console.ReadLine());
+string[] MonArray = MyArray(values);
+Console.Write("Исходный массив: ");
+PrintArray(MonArray);
+
+string[] NewArr = CalculationOfSymbol(MonArray);
+Console.Write("Итоговый массив: ");
+PrintArray(NewArr);
